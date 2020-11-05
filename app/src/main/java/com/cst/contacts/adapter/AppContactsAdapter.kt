72cf382e52b03c007bd02e505d1da179146807f7
Salome Kavtaradze <s_kavtaradze2@cu.edu.ge>
2020-11-05 20:21:41 +0400

    Implement random color

--- app/src/main/java/com/cst/contacts/adapter/AppContactsAdapter.kt
+++ app/src/main/java/com/cst/contacts/adapter/AppContactsAdapter.kt
@@ -1,12 +1,18 @@
 package com.cst.contacts.adapter
 
+import android.graphics.Color
+import android.graphics.LightingColorFilter
+import android.graphics.drawable.Drawable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.content.res.AppCompatResources
 import androidx.recyclerview.widget.RecyclerView
 import com.cst.contacts.R
 import com.cst.contacts.donottouch.ContactInfo
 import kotlinx.android.synthetic.main.contact_item.view.*
+import java.util.*
+
 
 class AppContactsAdapter(
     private val contacts: List<ContactInfo>
@@ -27,12 +33,23 @@ class AppContactsAdapter(
 
         fun setContent(contactInfo: ContactInfo) {
             with(contactInfo) {
+                val random = Random()
+                val iconBackground: Drawable? =
+                    AppCompatResources.getDrawable(itemView.context, R.drawable.circle)
+                val color: Int =
+                    Color.argb(255, random.nextInt(256), random.nextInt(256), random.nextInt(256))
+                iconBackground?.colorFilter = LightingColorFilter(color, color)
+
+                val firstChar = name[0].toString()
                 when {
                     adapterPosition != 0 && contacts[adapterPosition - 1].name[0] == name[0] ->
                         itemView.first_char.text = ""
-                    else -> itemView.first_char.text = name[0].toString()
+                    else -> itemView.first_char.text = firstChar
                 }
+
                 itemView.name.text = name
+                itemView.image.text = firstChar
+                itemView.image.background = iconBackground
             }
         }
     }
